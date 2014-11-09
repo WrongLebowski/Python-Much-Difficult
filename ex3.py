965da8bcd27fcda15dc949f7ff2716b801918bc2
WrongLebowski <caferacer941@gmail.com>
2014-11-09 08:25:03 -0700

    exercises from Learn Python The Hard Way

---
+++ ex3.py
@@ -0,0 +1,23 @@
+print "I will now count my chickens:"
+#calculates number of hens and roosters
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100 - 25 * 3.0 % 4.0
+
+print "Now I will count the eggs:"
+# calculates the number of eggs
+print 3 + 2 + 1 - 5 + 4.0 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+# determines true or false argument, this example would be false
+print 3 + 2 < 5 - 7
+# prints the text string then calculates the math
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+# prints the the true or false argument 
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
